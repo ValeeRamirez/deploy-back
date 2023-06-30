a0ce536d21c256b7b0204171c649f1b356adcadf
ValeeRamirez <v.ramirez@uc.cl>
2023-06-30 16:32:57 -0400

    otro cambio config.js

--- src/config/config.js
+++ src/config/config.js
@@ -13,14 +13,14 @@ module.exports = {
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_test`,
+    database: `${process.env.DB_NAME}`,
     host: process.env.DB_HOST,
     dialect: 'postgres',
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_production`,
+    database: `${process.env.DB_NAME}`,
     host: process.env.DB_HOST,
     dialect: 'postgres',
   },
